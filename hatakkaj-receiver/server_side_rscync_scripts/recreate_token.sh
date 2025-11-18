96c24b77969bcd71f1caea589c3e7000be86a70a
Samu Varjonen <5632161+sklvarjo@users.noreply.github.com>
2025-11-18 08:41:45 +0200

    Increased the default TTL of the token

--- hatakkaj-receiver/server_side_rscync_scripts/recreate_token.sh
+++ hatakkaj-receiver/server_side_rscync_scripts/recreate_token.sh
@@ -14,5 +14,5 @@ token=$(/usr/bin/cat $PATH/token.secret)
 
 $OCPATH/oc login $CLUSTER --token $token
 $OCPATH/oc project $PROJECT
-$OCPATH/oc create token hatakkaj-external-pipeline --duration=25h > $PATH/token.secret
+$OCPATH/oc create token hatakkaj-external-pipeline --duration=49h > $PATH/token.secret
 $OCPATH/oc logout
